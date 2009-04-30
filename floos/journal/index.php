9259ad04ae1aaa3aebfb0c5ca6ef384b8bc3ba98
Slim Amamou <slim.amamou@gmail.com>
2009-04-30 10:56:09 +0100

    j'ai amélioré la présentation du journal

--- floos/journal/index.php
+++ floos/journal/index.php
@@ -5,17 +5,31 @@
 
 $compte = Compte::charger($_GET['compte']);
 $ecritures = $compte->journal($_GET['debut'], $_GET['fin']);
+$ecritures->setFetchMode(PDO::FETCH_ASSOC);
 
-$html = "<table>";
+$html = <<<EOT
+<html>
+<head>
+<title>Journal comptable - FLOOS</title>
+</head>
+<body>
+<table>
+EOT;
+
 foreach ($ecritures as $e) {
 	$html .= "<tr>";
 	foreach ($e as $c) {
-		$html .= "<td>$c</td>";
+		$html .= "<td style='border: 1px solid silver; padding: 10px;'>$c</td>";
 	}
 	$html .= "</tr>";
 }
-$html .= "</table>";
-if (mail($compte->id, "Journal Comptable - FLOOS", $html)) {
+$html .= "</table></body></html>";
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+$headers .= 'From: FLOOS <floos@alixsys.com>' . "\r\n";
+
+if (mail($compte->id, "Journal Comptable", $html, $headers)) {
 	echo "<b>Un email vous a été envoyé</b>";
 } else {
 	echo "<b>Erreur d'envoi</b>";
