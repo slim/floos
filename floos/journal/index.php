f0452fcff0934b6359e113b3d62baba1ced475f8
Slim Amamou <slim.amamou@gmail.com>
2009-05-01 17:32:09 +0100

    meilleure présentation du rapport

--- floos/journal/index.php
+++ floos/journal/index.php
@@ -8,22 +8,58 @@ $ecritures = $compte->journal($_GET['debut'], $_GET['fin']);
 $ecritures->setFetchMode(PDO::FETCH_ASSOC);
 
 $html = <<<EOT
-<html>
-<head>
-<title>Journal comptable - FLOOS</title>
-</head>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>Journal comptable</title>
+<style type="text/css">
+
+table, tr, td, th {
+       margin: 0px;
+	   padding: 5px;
+       border-width: 0px;
+       border-spacing: 0px;
+       border-collapse: collapse;
+	   font-style: fixed;
+	   white-space: nowrap;
+}
+table tr td {border: solid 1px silver;}
+table tr th {border: solid 1px grey;}
+#error {
+	background-color: yellow;
+	border: 2px solid red;
+	padding: 10px;
+	margin: 10px;
+}
+pre {
+	color: silver;
+	background-color: black;
+	padding: 10px;
+	margin: 10px;
+}
+</style></head>
 <body>
 <table>
+<thead>
+<tr><th>date</th><th>catégorie</th><th>libellé</th><th>débit</th><th>crédit</th></tr>
+</thead>
+<tbody>
 EOT;
 
 foreach ($ecritures as $e) {
+	$date      = $e['date'];
+	$categorie = $e['categorie'];
+	$libelle   = $e['libelle'];
+	$debit     = $e['debit'];
+	$credit    = $e['credit'];
+
 	$html .= "<tr>";
-	foreach ($e as $c) {
-		$html .= "<td style='border: 1px solid silver; padding: 10px;'>$c</td>";
-	}
+	$html .= "<td>$date</td>";
+	$html .= "<td>$categorie</td>";
+	$html .= "<td>$libelle</td>";
+	$html .= "<td>$debit</td>";
+	$html .= "<td>$credit</td>";
 	$html .= "</tr>";
 }
-$html .= "</table></body></html>";
+$html .= "</tbody></table></body></html>";
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
