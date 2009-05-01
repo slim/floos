90950be49f5e47453ad8936f1cb2ff4ad4e346f3
Slim Amamou <slim.amamou@gmail.com>
2009-05-01 17:49:15 +0100

    bugfix: la categorie "test" etait != de "Test"

--- floos/cron/daily/index.php
+++ floos/cron/daily/index.php
@@ -2,4 +2,4 @@
 	$db = new PDO('sqlite:../../../floos.db');
 	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
 
-	$db->query("delete from journal_comptable where categorie='test'");
+	$db->query("delete from journal_comptable where categorie like 'test'");
